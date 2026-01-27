0650039de34e9496c800226fc4eb0b4c8cfc6c97
Nguyen Tuan Hai <nguyenhai0795426943@gmail.com>
2026-01-27 09:12:55 +0700

    add

---
+++ QuanLyBanHang/Form/frmLoaiSanPham.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace QuanLyBanHang.Forms
+{
+    partial class frmLoaiSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtTenLoai = new TextBox();
+            btnThem = new Button();
+            btnSua = new Button();
+            btnXoa = new Button();
+            btnLuu = new Button();
+            btnHuyBo = new Button();
+            btnThoat = new Button();
+            label2 = new Label();
+            dataGridView = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 117);
+            label1.Margin = new Padding(6, 0, 6, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(208, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Ten loai san pham";
+            // 
+            // txtTenLoai
+            // 
+            txtTenLoai.Location = new Point(347, 111);
+            txtTenLoai.Margin = new Padding(6, 6, 6, 6);
+            txtTenLoai.Name = "txtTenLoai";
+            txtTenLoai.Size = new Size(1089, 39);
+            txtTenLoai.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(347, 194);
+            btnThem.Margin = new Padding(6, 6, 6, 6);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(139, 49);
+            btnThem.TabIndex = 2;
+            btnThem.Text = "Them";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(542, 194);
+            btnSua.Margin = new Padding(6, 6, 6, 6);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(139, 49);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sua";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(737, 194);
+            btnXoa.Margin = new Padding(6, 6, 6, 6);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(139, 49);
+            btnXoa.TabIndex = 4;
+            btnXoa.Text = "Xoa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(919, 194);
+            btnLuu.Margin = new Padding(6, 6, 6, 6);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(139, 49);
+            btnLuu.TabIndex = 5;
+            btnLuu.Text = "Luu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // btnHuyBo
+            // 
+            btnHuyBo.Location = new Point(1118, 194);
+            btnHuyBo.Margin = new Padding(6, 6, 6, 6);
+            btnHuyBo.Name = "btnHuyBo";
+            btnHuyBo.Size = new Size(139, 49);
+            btnHuyBo.TabIndex = 6;
+            btnHuyBo.Text = "Huy Bo";
+            btnHuyBo.UseVisualStyleBackColor = true;
+            btnHuyBo.Click += btnHuyBo_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(1300, 194);
+            btnThoat.Margin = new Padding(6, 6, 6, 6);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(139, 49);
+            btnThoat.TabIndex = 7;
+            btnThoat.Text = "Thoat";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 23);
+            label2.Margin = new Padding(6, 0, 6, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(229, 32);
+            label2.TabIndex = 8;
+            label2.Text = "Thong tin san pham";
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(56, 316);
+            dataGridView.Margin = new Padding(6, 6, 6, 6);
+            dataGridView.Name = "dataGridView";
+            dataGridView.ReadOnly = true;
+            dataGridView.RowHeadersWidth = 82;
+            dataGridView.Size = new Size(1384, 533);
+            dataGridView.TabIndex = 9;
+            // 
+            // frmLoaiSanPham
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1486, 960);
+            Controls.Add(dataGridView);
+            Controls.Add(label2);
+            Controls.Add(btnThoat);
+            Controls.Add(btnHuyBo);
+            Controls.Add(btnLuu);
+            Controls.Add(btnXoa);
+            Controls.Add(btnSua);
+            Controls.Add(btnThem);
+            Controls.Add(txtTenLoai);
+            Controls.Add(label1);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "frmLoaiSanPham";
+            Text = "frmLoaiSanPham";
+            Load += frmLoaiSanPham_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtTenLoai;
+        private Button btnThem;
+        private Button btnSua;
+        private Button btnXoa;
+        private Button btnLuu;
+        private Button btnHuyBo;
+        private Button btnThoat;
+        private Label label2;
+        private DataGridView dataGridView;
+    }
+}
