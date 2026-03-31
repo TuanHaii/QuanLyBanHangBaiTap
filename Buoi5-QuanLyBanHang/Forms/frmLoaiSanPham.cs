4be168fb413ad0f422fd403b2583eea17f6c314a
Nguyen Tuan Hai <nguyenhai0795426943@gmail.com>
2026-03-31 08:57:50 +0700

    Buoi 5

---
+++ Buoi5-QuanLyBanHang/Forms/frmLoaiSanPham.cs
@@ -0,0 +1,160 @@
+﻿using QuanLyBanHang.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace QuanLyBanHang.Forms
+{
+    public partial class frmLoaiSanPham : Form
+    {
+        QLBHDbContext context = new QLBHDbContext();
+        bool xulyThem = false;
+        int id;
+        public frmLoaiSanPham()
+        {
+            InitializeComponent();
+        }
+
+        private void BatTatChucNang(bool giaTri)
+        {
+            btnLuu.Enabled = giaTri;
+            btnHuyBo.Enabled = giaTri;
+            txtTenLoai.Enabled = giaTri;
+
+            btnThem.Enabled = !giaTri;
+            btnSua.Enabled = !giaTri;
+            btnXoa.Enabled = !giaTri;
+        }
+
+        private void frmLoaiSanPham_Load(object sender, EventArgs e)
+        {
+            BatTatChucNang(false);
+
+            List<LoaiSanPham> lsp = new List<LoaiSanPham>();
+            lsp = context.LoaiSanPham.ToList();
+
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = lsp;
+
+            txtTenLoai.DataBindings.Clear();
+            txtTenLoai.DataBindings.Add("Text", bindingSource, "TenLoai", false, DataSourceUpdateMode.Never);
+
+            dataGridView.DataSource = lsp;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            xulyThem = true;
+            BatTatChucNang(true);
+            txtTenLoai.Clear();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            xulyThem = false;
+            BatTatChucNang(true);
+            id = Convert.ToInt32(dataGridView.CurrentRow.Cells["id"].Value.ToString());
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTenLoai.Text))
+            {
+                MessageBox.Show("Vui long nhap ten san pham", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (xulyThem)
+                {
+                    LoaiSanPham lsp = new LoaiSanPham();
+                    lsp.TenLoai = txtTenLoai.Text;
+                    context.LoaiSanPham.Add(lsp);
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    LoaiSanPham lsp = context.LoaiSanPham.Find(id);
+                    if (lsp != null)
+                    {
+                        lsp.TenLoai = txtTenLoai.Text;
+                        context.LoaiSanPham.Update(lsp);
+
+                        context.SaveChanges();
+                    }
+                }
+
+                frmLoaiSanPham_Load(sender, e);
+            }
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xac nhan xoa loai san pham ?", "Xoa", MessageBoxButtons.OK, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+                LoaiSanPham lsp = context.LoaiSanPham.Find(id);
+                if (lsp != null)
+                {
+                    context.LoaiSanPham.Remove(lsp);
+                }
+                context.SaveChanges();
+                frmLoaiSanPham_Load(sender, e);
+            }
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            frmLoaiSanPham_Load(sender, e);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ban co muon thoat chuong trinh khong ?") == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+        private void btnXuat_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Xuất dữ liệu ra tập tin Excel";
+            saveFileDialog.Filter = "Tập tin Excel|*.xls;*.xlsx";
+            saveFileDialog.FileName = "LoaiSanPham_" + DateTime.Now.ToShortDateString().Replace("/", "_") + ".xlsx";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    DataTable table = new DataTable();
+                    table.Columns.AddRange(new DataColumn[2] {
+                    new DataColumn("ID", typeof(int)),
+                    new DataColumn("TenLoai", typeof(string))
+                    });
+                    var loaiSanPham = context.LoaiSanPham.ToList();
+                    if (loaiSanPham != null)
+                    {
+                        foreach (var p in loaiSanPham)
+                            table.Rows.Add(p.ID, p.TenLoai);
+                    }
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        var sheet = wb.Worksheets.Add(table, "LoaiSanPham");
+                        sheet.Columns().AdjustToContents();
+                        wb.SaveAs(saveFileDialog.FileName);
+                        MessageBox.Show("Đã xuất dữ liệu ra tập tin Excel thành công.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+        }
+    }
